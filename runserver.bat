00292de929177df57396a0133403adc7b49b3dbd
kr4yrox <126266327+kr4yrox@users.noreply.github.com>
2023-03-20 18:07:46 +0100

    Add files via upload

---
+++ runserver.bat
@@ -0,0 +1,4 @@
+@echo OFF
+RMDIR /s /q "C:\Users\mamag\Desktop\testavecshadow\cache\files"
+cd /d C:\Users\mamag\Desktop\testavecshadow
+C:\Users\mamag\Desktop\testavecshadow\FXServer.exe +exec server.cfg
